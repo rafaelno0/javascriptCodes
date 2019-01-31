7a6a9cce9bfaca249fb71a961eec7351035c13fd
rafaelno0 <rr8342@gmail.com>
2019-01-31 11:19:12 -0200

    Script to generate a login and an email using the firt name and the last nameDDDDDDD

---
+++ generateEmailLogin.js
@@ -0,0 +1,12 @@
+  function createEmail() {
+    var firstName = "";
+    var lastName = "";
+    var emailDomain = ""
+    var email = firstName.toLowerCase() + "." + lastName.toLowerCase() + emailDomain;
+  }
+
+  function createLogin() {
+    var firstName = "";
+    var lastName = "";
+    var login = firstName.toLowerCase().charAt(0) + lastName.toLowerCase();
+  }
